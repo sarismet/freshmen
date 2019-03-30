2ec587fe8e12981cc0d05dc9f21e7b6b8af8eb07
İsmet Sarı <49066101+sarismet@users.noreply.github.com>
2019-03-30 22:55:05 +0300

    Update Action.java

--- Snake/src/naturesimulator/Action.java
+++ Snake/src/naturesimulator/Action.java
@@ -3,17 +3,9 @@ package naturesimulator;
 import game.Direction;
 
 
-/**
- * Class representing the possible actions for Nature Simulator game
- *
- * IMPORTANT: Please do not modify this class in any way,
- * it will be reset when grading your project.
- */
+
 public class Action {
 
-    /**
-     * Enum representing possible action types
-     */
     public enum Type {
         MOVE,
         REPRODUCE,
@@ -24,11 +16,7 @@ public class Action {
     private final Type type;
     private final Direction direction;
 
-    /**
-     * Creates an action without direction
-     * @throws IllegalArgumentException in case the action type requires a direction
-     * @param type action type
-     */
+
     public Action(Type type) {
         if (type == Type.MOVE || type == Type.ATTACK || type == Type.REPRODUCE) {
             throw new IllegalArgumentException("You cannot create an action "
@@ -38,12 +26,7 @@ public class Action {
         direction = null;
     }
 
-    /**
-     * Creates an action with direction
-     * @throws IllegalArgumentException in case the action type must not have a direction
-     * @param type action type
-     * @param direction direction
-     */
+
     public Action(Type type, Direction direction) {
         if (type == Type.STAY) {
             throw new IllegalArgumentException("Action of type "
@@ -53,18 +36,12 @@ public class Action {
         this.direction = direction;
     }
 
-    /**
-     * Getter for the type of the action
-     * @return action type
-     */
+
     public Type getType() {
         return type;
     }
 
-    /**
-     * Getter for the direction of the action
-     * @return direction or null if a direction does not exist
-     */
+
     public Direction getDirection() {
         return direction;
     }
