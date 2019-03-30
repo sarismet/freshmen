0d36d47e3e567e72beed3a7bd1d8d4690a814c3e
İsmet Sarı <49066101+sarismet@users.noreply.github.com>
2019-03-30 22:50:55 +0300

    Update ApplicationWindow.java

--- Forests/src/ui/ApplicationWindow.java
+++ Forests/src/ui/ApplicationWindow.java
@@ -2,13 +2,6 @@ package ui;
 
 import javax.swing.*;
 
-
-/**
- * The main application window.
- *
- * IMPORTANT: Please do not modify this class in any way,
- * it will be reset when grading your project.
- */
 public class ApplicationWindow {
 
 	private JFrame frame;
